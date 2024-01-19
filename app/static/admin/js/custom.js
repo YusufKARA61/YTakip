0412f1ea7e384dc937a9fb0fe1574c022d52e1e2
YusufKARA61 <81572942+YusufKARA61@users.noreply.github.com>
2024-01-19 17:05:44 +0300

    1901Ofis
    Harita eklendi

--- app/static/admin/js/custom.js
+++ app/static/admin/js/custom.js
@@ -52,3 +52,64 @@ function closeMessageModal() {
   $('#message-modal').modal('hide');
   window.location.reload(); // Sayfayı yenile
 }
+
+function initMap() {
+  var map = new ol.Map({
+    target: 'mapContainer',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([28.8547, 41.0345]), // İstanbul'un koordinatları
+      zoom: 13 // Yakınlaştırma seviyesi
+    })
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initMap();
+});
+
+function initMap() {
+  // Harita
+  var map = new ol.Map({
+    target: 'mapContainer',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }),
+      new ol.layer.Vector({
+        source: new ol.source.Vector({
+          format: new ol.format.GeoJSON(),
+          url: 'URL_TO_YOUR_POSTGIS_VECTOR_DATA' // PostGIS veritabanınızın URL'sini buraya ekleyin
+        }),
+        style: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'blue',
+            width: 2
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(0, 0, 255, 0.1)'
+          })
+        })
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([28.8547, 41.0345]), // İstanbul'un koordinatları
+      zoom: 13 // Yakınlaştırma seviyesi
+    })
+  });
+
+  // Harita üzerinde ada parsellerini göstermek için uygun bir stil ve kaynak kullanın.
+  // Burada stil ve kaynak ayarlarını PostGIS veritabanınıza ve ada parsellerinin formatına göre özelleştirmeniz gerekecektir.
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initMap();
+});
+
+
+
+
